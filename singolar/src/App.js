1e705692370b93f038fd64e91d52e4aa703bd266
Felipe Sena <felipe.desena@hotmail.com>
2022-09-22 17:04:38 -0300

    feat: globals_Styles

--- singolar/src/App.js
+++ singolar/src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
 import MainModal from "./components/MainModal";
 import { ModalProvider } from "./components/Providers/ModalProvider";
 import MainRoutes from "./components/Routes";
+import GlobalStyle from "./components/Styles/Globals";
 
 function App() {
   return (
     <ModalProvider>
+      <GlobalStyle />
       <MainModal />
       <MainRoutes />
     </ModalProvider>

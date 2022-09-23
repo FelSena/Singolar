14d66565295e4eafcf20e5b7f1edc9a8f1f7ec8e
Felipe Sena <felipe.desena@hotmail.com>
2022-09-22 23:45:08 -0300

    fixed: css margin

--- singolar/src/App.js
+++ singolar/src/App.js
@@ -2,13 +2,11 @@ import { ToastContainer } from "react-toastify";
 import MainModal from "./components/MainModal";
 import { ModalProvider } from "./components/Providers/ModalProvider";
 import MainRoutes from "./components/Routes";
-import GlobalStyle from "./components/Styles/Globals";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <ModalProvider>
-      <GlobalStyle />
       <ToastContainer
         position="top-center"
         autoClose={2000}
